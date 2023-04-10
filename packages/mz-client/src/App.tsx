127367ef0fd4fa4964984cb8e60c1e9a8109dc67
shseok <shseok0674@gmail.com>
2023-04-10 17:50:10 +0900

    init: react project

---
+++ packages/mz-client/src/App.tsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import Home from '~/pages/Home';
+import Error from '~/pages/Error';
+import Register from '~/pages/Register';
+import Login from '~/pages/Login';
+import Header from '~/components/Header';
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div className='layout'>
+        <Outlet />
+      </div>
+      {/*<Footer />*/}
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  // {
+  //   path: '/error',
+  //   element: <NetworkError />,
+  // },
+]);
+
+function App() {
+  return (
+    <div className='app'>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
