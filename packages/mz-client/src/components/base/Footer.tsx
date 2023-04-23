30e61915c746e0b0d25ba5851dbc7397bac24f10
shseok <shseok0674@gmail.com>
2023-04-23 20:57:24 +0900

    refactor: use NavLink

--- packages/mz-client/src/components/base/Footer.tsx
+++ packages/mz-client/src/components/base/Footer.tsx
@@ -1,30 +1,15 @@
-import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { colors } from '~/lib/colors';
 import FooterTabItem from '~/components/base/FooterTabItem';
-import { useLocation } from 'react-router-dom';
-
-const paths = ['search', 'bookmarks', 'setting'] as const;
-
-const isValidPath = (path): path is (typeof paths)[number] => paths.includes(path);
 
 const Footer = () => {
-  const location = useLocation();
-  const currentPage = useMemo(() => {
-    const path = location.pathname.split('/')[1];
-    if (isValidPath(path)) {
-      return path;
-    }
-    return 'home';
-  }, [location.pathname]);
-  console.log(currentPage);
   return (
     <StyledFooter>
-      <FooterTabItem icon={'home'} isActive={currentPage === 'home'} to='/' />
-      <FooterTabItem icon={'search'} isActive={currentPage === 'search'} to='/search' />
+      <FooterTabItem icon={'home'} to='/' />
+      <FooterTabItem icon={'search'} to='/search' />
       <FooterTabItem icon={'plus-circle'} />
-      <FooterTabItem icon={'bookmark'} isActive={currentPage === 'bookmarks'} to='/bookmarks' />
-      <FooterTabItem icon={'setting'} isActive={currentPage === 'setting'} to='/setting' />
+      <FooterTabItem icon={'bookmark'} to='/bookmarks' />
+      <FooterTabItem icon={'setting'} to='/setting' />
     </StyledFooter>
   );
 };
--- packages/mz-client/src/components/base/FooterTabItem.tsx
+++ packages/mz-client/src/components/base/FooterTabItem.tsx
@@ -6,7 +6,7 @@ import { ReactComponent as plusCircle } from '~/assets/plusCircle.svg';
 import { ReactComponent as Bookmark } from '~/assets/bookmark.svg';
 import { ReactComponent as Setting } from '~/assets/setting.svg';
 import { colors } from '~/lib/colors';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const IconMap = {
   home: Feed,
@@ -18,15 +18,17 @@ const IconMap = {
 
 interface Props {
   icon: keyof typeof IconMap;
-  isActive?: boolean;
   to?: string;
 }
 
-const FooterTabItem = ({ icon, isActive, to }: Props) => {
+const FooterTabItem = ({ icon, to }: Props) => {
   const iconEl = React.createElement(IconMap[icon]);
   if (to) {
     return (
-      <LinkItem $isActive={isActive} to={to}>
+      <LinkItem
+        to={to}
+        className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}
+      >
         {iconEl}
       </LinkItem>
     );
@@ -34,7 +36,7 @@ const FooterTabItem = ({ icon, isActive, to }: Props) => {
   return <ButtonItem>{iconEl}</ButtonItem>;
 };
 
-const CommonStyle = (isActive?: boolean) => css`
+const CommonStyle = css`
   flex: 1;
   display: flex;
   justify-content: center;
@@ -43,15 +45,16 @@ const CommonStyle = (isActive?: boolean) => css`
     width: 32px;
     height: 32px;
     color: ${colors.gray2};
-    ${isActive &&
-    css`
+  }
+  &.active {
+    svg {
       color: ${colors.primary};
-    `}
+    }
   }
 `;
 
-const LinkItem = styled(Link)<{ $isActive?: boolean }>`
-  ${(props) => CommonStyle(props.$isActive)}
+const LinkItem = styled(NavLink)`
+  ${CommonStyle}
 `;
 
 const ButtonItem = styled.button`
@@ -59,15 +62,7 @@ const ButtonItem = styled.button`
   outline: none;
   border: none;
 
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  svg {
-    width: 32px;
-    height: 32px;
-    color: ${colors.gray2};
-  }
+  ${CommonStyle}
 `;
 
 export default FooterTabItem;
