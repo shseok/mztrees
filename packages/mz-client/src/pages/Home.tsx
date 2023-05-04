4ea2cb2ba4e5d2b40c383acf7a2521b8d9445554
shseok <shseok0674@gmail.com>
2023-05-04 10:21:52 +0900

    fix: infinite scroll not work

--- packages/mz-client/src/pages/Home.tsx
+++ packages/mz-client/src/pages/Home.tsx
@@ -9,39 +9,43 @@ const Home = () => {
   const ref = useRef<HTMLDivElement>(null);
   const fetchNext = useCallback(async () => {
     const { endCursor, hasNextPage } = pages.at(-1)?.pageInfo ?? {
-      endCursor: null,
+      endCursor: undefined,
       hasNextPage: false,
     };
-    if (!hasNextPage || !endCursor) return;
+    // 다음에 가져올 것이 없음
+    if (endCursor === null) return;
+    console.log('fetchNext');
     const result = await getItems(endCursor);
     setPages((prev) => [...prev, result]);
   }, [pages]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getItems();
-      // setData(data);
-      setPages([data]);
-    };
-    console.log('fetching');
-    //
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const data = await getItems();
+  //     // setData(data);
+  //     setPages([data]);
+  //   };
+  //   console.log('fetching');
+  //   //
+  //   fetchData();
+  // }, []);
 
   useEffect(() => {
     if (!ref.current) return;
     const observer = new IntersectionObserver(
       (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            fetchNext();
-          }
-        });
+        if (entries[0].isIntersecting) {
+          fetchNext();
+        }
+        // entries.forEach((entry) => {
+        //   if (entry.isIntersecting) {
+        //     fetchNext();
+        //   }
+        // });
       },
       {
-        root: ref.current.parentElement,
-        rootMargin: '64px',
-        threshold: 1,
+        rootMargin: '100px',
+        threshold: 1.0,
       },
     );
     observer.observe(ref.current);
@@ -53,13 +57,12 @@ const Home = () => {
   const items = pages ? pages.flatMap((page) => page.list) : null;
   // console.log('out', items);
   if (!items) {
-    return <div>data is not exist</div>;
+    return <div>Loading...</div>;
   }
   return (
     <>
       <LinkCardList items={items} />
-      <div ref={ref}>heeeeeeeeeeeeeeeeeellooo</div>
-      <div>aaaaaaaaaaaa</div>
+      <div ref={ref} />
     </>
   );
 };
