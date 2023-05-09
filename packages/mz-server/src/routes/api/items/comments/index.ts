a07b9263622a5fc2c1d1e717b67c0d149a93b9e6
shseok <shseok0674@gmail.com>
2023-05-09 15:07:27 +0900

    refactor: type circus

--- packages/mz-server/src/routes/api/items/comments/index.ts
+++ packages/mz-server/src/routes/api/items/comments/index.ts
@@ -4,16 +4,16 @@ import { CommentsRoute, CommentsRouteSchema } from './schema.js'
 export const commentsRoute: FastifyPluginAsync = async (fastify) => {
   fastify.get<CommentsRoute['GetComments']>(
     '/',
-    { schema: CommentsRouteSchema['GetComments'] },
+    { schema: CommentsRouteSchema.GetComments },
     async (request) => {
-      console.log(request.params.id)
+      console.log(request.params?.id)
       return 'hello world'
     },
   )
 
   fastify.post<CommentsRoute['CreateComment']>(
     '/',
-    { schema: CommentsRouteSchema['CreateComment'] },
+    { schema: CommentsRouteSchema.CreateComment },
     async (request) => {
       const { id } = request.params
       const { text } = request.body
--- packages/mz-server/src/routes/api/items/comments/schema.ts
+++ packages/mz-server/src/routes/api/items/comments/schema.ts
@@ -1,14 +1,12 @@
-import { Static, Type } from '@sinclair/typebox'
-import { ItemParamsSchema, ItemParamsType, ItemSchema } from '../schema.js'
+import { Type } from '@sinclair/typebox'
+import { ItemParamsSchema } from '../schema.js'
 import { Nullable } from '../../../../lib/typebox.js'
-import { createRouteSchema } from '../../../../lib/routeSchema.js'
+import { RoutesType, createRouteSchema } from '../../../../lib/routeSchema.js'
 
 const CreateCommentBodySchema = Type.Object({
   text: Type.String(),
   parentCommentId: Nullable(Type.Integer()),
 })
-
-type CreateCommentBodyType = Static<typeof CreateCommentBodySchema>
 
 // 대댓글 작성, 대댓글 리스팅, 좋아요에 사용
 const CommentsParamsSchema = Type.Object({
@@ -16,20 +14,9 @@ const CommentsParamsSchema = Type.Object({
   commentId: Type.Integer(),
 })
 
-type CommentsParamsType = Static<typeof CommentsParamsSchema>
-
 const UpdateCommentSchema = Type.Object({
   text: Type.String(),
 })
-
-type UpdateCommentType = Static<typeof UpdateCommentSchema>
-
-/*
-export const GetCommentSchema: FastifySchema = {
-  params: ItemParamsSchema,
-}
-아래 와 같이 타입 확장을 통해 리팩토링
-*/
 
 export const CommentsRouteSchema = createRouteSchema({
   GetComments: {
@@ -57,28 +44,36 @@ export const CommentsRouteSchema = createRouteSchema({
   },
 })
 
-export type CommentsRoute = {
-  GetComments: {
-    Params: ItemParamsType
-  }
-  CreateComment: {
-    Params: ItemParamsType
-    Body: CreateCommentBodyType
-  }
-  GetSubcomments: {
-    Params: CommentsParamsType
-  }
-  LikeComment: {
-    Params: CommentsParamsType
-  }
-  UnlikeComment: {
-    Params: CommentsParamsType
-  }
-  DeleteComment: {
-    Params: CommentsParamsType
-  }
-  UpdateComment: {
-    Params: CommentsParamsType
-    Body: UpdateCommentType
-  }
-}
+export type CommentsRoute = RoutesType<typeof CommentsRouteSchema>
+
+// 리팩토링: 아래의 여러 코드 > 위의 한줄 코드
+
+// type CreateCommentBodyType = Static<typeof CreateCommentBodySchema>
+// type CommentsParamsType = Static<typeof CommentsParamsSchema>
+// type UpdateCommentType = Static<typeof UpdateCommentSchema>
+
+// export type CommentsRoute = {
+//   GetComments: {
+//     Params: ItemParamsType
+//   }
+//   CreateComment: {
+//     Params: ItemParamsType
+//     Body: CreateCommentBodyType
+//   }
+//   GetSubcomments: {
+//     Params: CommentsParamsType
+//   }
+//   LikeComment: {
+//     Params: CommentsParamsType
+//   }
+//   UnlikeComment: {
+//     Params: CommentsParamsType
+//   }
+//   DeleteComment: {
+//     Params: CommentsParamsType
+//   }
+//   UpdateComment: {
+//     Params: CommentsParamsType
+//     Body: UpdateCommentType
+//   }
+// }
