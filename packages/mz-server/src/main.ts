0b16e64f8759c06bdb55003f747bc0f84414140b
shseok <shseok0674@gmail.com>
2023-07-03 21:01:28 +0900

    feat: config server origin

--- packages/mz-server/src/main.ts
+++ packages/mz-server/src/main.ts
@@ -18,12 +18,7 @@ console.log(process.env.NODE_ENV)
 
 if (process.env.NODE_ENV === 'development') {
   server.register(cors, {
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:4000',
-      'http://127.0.0.1:5173',
-      'http://localhost:5173',
-    ],
+    origin: /localhost/,
     credentials: true,
     allowedHeaders: ['Cookie', 'Content-Type'],
     // exposedHeaders: ['Set-Cookie'],
