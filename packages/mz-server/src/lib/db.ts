129fedf69dd51bad1318da3c773e401f6a231c28
shseok <shseok0674@gmail.com>
2023-07-14 12:00:16 +0900

    refactor: reuse prismaClient in development env

--- packages/mz-server/src/lib/db.ts
+++ packages/mz-server/src/lib/db.ts
@@ -1,5 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
-const db = new PrismaClient()
+declare global {
+  var cachedPrisma: PrismaClient
+}
+
+let prisma: PrismaClient
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient()
+  }
+  prisma = global.cachedPrisma
+}
+
+const db = prisma
 
 export default db
