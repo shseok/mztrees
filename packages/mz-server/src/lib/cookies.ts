ffa6c236b6c7a24ff9631e9bff85636f5fdb3d2d
shseok <shseok0674@gmail.com>
2023-07-21 21:06:56 +0900

    feat: set cookie on sub domain

--- packages/mz-server/src/lib/cookies.ts
+++ packages/mz-server/src/lib/cookies.ts
@@ -1,22 +1,31 @@
 import { FastifyReply } from 'fastify'
 
+const domains =
+  process.env.NODE_ENV === 'production' ? ['.mztrees.com'] : [undefined]
+
 export function setTokenCookie(
   reply: FastifyReply,
   tokens: { accessToken: string; refreshToken: string },
 ) {
-  reply.setCookie('access_token', tokens.accessToken, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 60 * 60),
-    path: '/',
-  })
-  reply.setCookie('refresh_token', tokens.refreshToken, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
-    path: '/',
+  domains.forEach((domain) => {
+    reply.setCookie('access_token', tokens.accessToken, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 60 * 60),
+      path: '/',
+      domain,
+    })
+    reply.setCookie('refresh_token', tokens.refreshToken, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+      path: '/',
+      domain,
+    })
   })
 }
 
 export function clearTokenCookie(reply: FastifyReply) {
-  reply.clearCookie('access_token')
-  reply.clearCookie('refresh_token')
+  domains.forEach((domain) => {
+    reply.clearCookie('access_token', { domain, path: '/' })
+    reply.clearCookie('refresh_token', { domain, path: '/' })
+  })
 }
