4d70aff426eee3e5d001709d3b5c02d866db9606
shseok <shseok0674@gmail.com>
2023-08-13 21:36:27 +0900

    fix: change header width in desktop mode

--- packages/mz-client/src/components/base/DesktopHeader.tsx
+++ packages/mz-client/src/components/base/DesktopHeader.tsx
@@ -14,26 +14,28 @@ const DesktopHeader = () => {
 
   return (
     <header className={styles.block}>
-      <Link className={styles.home_link} href="/">
-        <Logo className={styles.styled_logo} />
-      </Link>
-      <div className={styles.content}>
-        <div className={styles.addon}></div>
-        <div className={styles.addon}>
-          <ThemeToggleButton />
-          <SearchArea />
-          {currentUser ? (
-            <UserAddon username={currentUser.username} profileImage={""} />
-          ) : (
-            <div className={styles.buttons}>
-              <Button variant="tertiary" size="small" to="/login">
-                로그인
-              </Button>
-              <Button size="small" to="/register">
-                회원가입
-              </Button>
-            </div>
-          )}
+      <div className={styles.block_inner}>
+        <Link className={styles.home_link} href="/">
+          <Logo className={styles.styled_logo} />
+        </Link>
+        <div className={styles.content}>
+          <div className={styles.addon}></div>
+          <div className={styles.addon}>
+            <ThemeToggleButton />
+            <SearchArea />
+            {currentUser ? (
+              <UserAddon username={currentUser.username} profileImage={""} />
+            ) : (
+              <div className={styles.buttons}>
+                <Button variant="tertiary" size="small" to="/login">
+                  로그인
+                </Button>
+                <Button size="small" to="/register">
+                  회원가입
+                </Button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </header>
