051727fafca17121e3e5528549e45de3127219ba
shseok <shseok0674@gmail.com>
2023-09-07 01:27:03 +0900

    fix: [WIP] certain sites do not pass URL validation in deployment env

--- packages/mz-server/src/lib/extractPageInfo.ts
+++ packages/mz-server/src/lib/extractPageInfo.ts
@@ -70,7 +70,12 @@ async function validateUrl(url: string): Promise<ValidateResult> {
 
   if (hasProtocol) {
     try {
-      const { data } = await client.get(url)
+      const { data } = await client.get(url, {
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        },
+      })
 
       return {
         url,
