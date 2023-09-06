824d0a8740250b0b84021605a8b8e4c261112c92
shseok <shseok0674@gmail.com>
2023-09-06 23:59:30 +0900

    fix: add tag data

--- packages/mz-server/prisma/seed.ts
+++ packages/mz-server/prisma/seed.ts
@@ -19,6 +19,17 @@ async function main() {
       { name: '검색' },
       { name: '문서' },
       { name: '부동산' },
+      { name: '뉴스' },
+      { name: '게임' },
+      { name: '여행' },
+      { name: '건강' },
+      { name: '음식' },
+      { name: '기술' },
+      { name: '자동차' },
+      { name: '지도' },
+      { name: '예술' },
+      { name: '동물' },
+      { name: '환경' },
     ],
   })
 }
