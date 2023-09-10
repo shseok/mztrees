d170c5446061c6e3cc3bb7d0fc49f8e0230cd3db
shseok <shseok0674@gmail.com>
2023-09-10 15:12:09 +0900

    fix: customize useSetSearchParams

--- packages/mz-client/src/hooks/useSetSearchParams.ts
+++ packages/mz-client/src/hooks/useSetSearchParams.ts
@@ -2,30 +2,30 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import { useCallback } from "react";
-// import useSearchParams from "./useSearchParams";
+import useSearchParams from "./useSearchParams";
 
 // Get a new searchParams string by merging the current searchParams with a provided key/value pair
+// ex) setSearchParams({mode: '...', tag: '...' | null})
 export default function useSetSearchParams() {
   const router = useRouter();
   const pathname = usePathname();
-  // const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
 
-  // REFACTOR: useSearchParams를 사용하여 getAll, entries등을 이용
   const setSearchParams = useCallback(
-    (obj: Record<string, string>) => {
-      const createQueryString = (name: string, value: string) => {
-        const params = new URLSearchParams();
-        // const params = new URLSearchParams(searchParams);
-        params.set(name, value);
-        return params.toString();
-      };
-      const query = [];
+    (obj: Record<string, string | null>) => {
+      const currentParams = Object.fromEntries<string | null>(
+        searchParams.entries()
+      );
       for (const [key, value] of Object.entries(obj)) {
-        const param = createQueryString(key, value);
-        query.push(param);
+        currentParams[key] = value;
       }
-      const parameters = query.join("&");
-      router.push(pathname + "?" + parameters);
+      console.log(currentParams);
+      let query = [];
+      for (const [key, value] of Object.entries(currentParams)) {
+        if (!value) continue;
+        query.push(`${key}=${value}`);
+      }
+      router.push(pathname + "?" + query.join("&"));
     },
     [router, pathname]
   );
