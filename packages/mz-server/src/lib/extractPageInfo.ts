9c10408ac4d2cffaf3a374233490fda32bde4c1c
shseok <shseok0674@gmail.com>
2023-09-18 15:58:13 +0900

    fix: remove user-agent in header

--- packages/mz-server/src/lib/extractPageInfo.ts
+++ packages/mz-server/src/lib/extractPageInfo.ts
@@ -70,12 +70,7 @@ async function validateUrl(url: string): Promise<ValidateResult> {
 
   if (hasProtocol) {
     try {
-      const { data } = await client.get(url, {
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-        },
-      })
+      const { data } = await client.get(url)
 
       return {
         url,
