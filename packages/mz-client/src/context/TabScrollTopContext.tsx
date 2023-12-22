8804f69c6859ec8d2f679a1d852014dfa2d13051
shseok <shseok0674@gmail.com>
2023-12-23 01:56:22 +0900

    refactor: useTabScrollTop

--- packages/mz-client/src/context/TabScrollTopContext.tsx
+++ packages/mz-client/src/context/TabScrollTopContext.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { usePathname } from "next/navigation";
-import { createContext, useContext, useEffect, useRef } from "react";
+import { usePathname } from 'next/navigation';
+import { createContext, useContext, useEffect, useRef } from 'react';
 
 export const TabScrollTopContext = createContext<Map<string, number> | null>(
   null
@@ -23,11 +23,11 @@ export function TabScrollTopContextProvider({
 export function useTabScrollTop(ref: React.RefObject<HTMLElement>) {
   const context = useContext(TabScrollTopContext);
   const pathname = usePathname();
-  const mountRef = useRef(false);
+  const mountedRef = useRef(false);
 
   if (!context) {
     throw new Error(
-      "useTabScrollTop must be used within a TabScrollTopContextProvider"
+      'useTabScrollTop must be used within a TabScrollTopContextProvider'
     );
   }
 
@@ -39,13 +39,19 @@ export function useTabScrollTop(ref: React.RefObject<HTMLElement>) {
       context.set(pathname, current.scrollTop);
     };
 
-    current.addEventListener("scroll", handleScroll);
-
-    // 스크롤 위치를 이전에 저장된 위치로 설정
-    current.scrollTop = context.get(pathname) ?? 0;
+    current.addEventListener('scroll', handleScroll);
 
     return () => {
-      current.removeEventListener("scroll", handleScroll);
+      current.removeEventListener('scroll', handleScroll);
     };
   }, [context, ref, pathname]);
+
+  // 컴포넌트가 처음 마운트될 때만 초기 스크롤 위치를 설정하고, 그 이후에는 해당 작업을 수행하지 않도록 하는 패턴 (twice > once)
+  useEffect(() => {
+    if (mountedRef.current) return;
+    mountedRef.current = true;
+    if (ref.current) {
+      ref.current.scrollTop = context.get(pathname) ?? 0;
+    }
+  }, [pathname, context, ref]);
 }
