972f331e66eb4f864d359a48f8c6b59d02dbf116
shseok <shseok0674@gmail.com>
2023-12-23 21:42:48 +0900

    fix: limit search card body & add search result count

--- packages/mz-client/src/components/search/SearchResultCardList.tsx
+++ packages/mz-client/src/components/search/SearchResultCardList.tsx
@@ -10,9 +10,12 @@ interface Props {
 const SearchResultCardList = ({ items, searchText }: Props) => {
   return (
     <div className={styles.block}>
-      <h2
-        className={styles.search_text}
-      >{`'${searchText}'에 대한 검색 결과`}</h2>
+      <div className={styles.header}>
+        <h2 className={styles.title}>{`'${searchText}'에 대한 검색 결과`}</h2>
+        <p className={styles.count}>
+          총 <strong>{`${items.length}개`}</strong>의 포스트가 검색되었습니다.
+        </p>
+      </div>
       {items.map((item) => (
         <SearchResultCard item={item} key={item.id} />
       ))}
