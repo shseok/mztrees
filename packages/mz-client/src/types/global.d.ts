63c26bdeb48a38a1177b82d5dd7a025b98f94ba6
shseok <shseok0674@gmail.com>
2024-01-12 23:58:31 +0900

    feat(types): add BeforeInstallPromptEvent type declaration

--- packages/mz-client/src/types/global.d.ts
+++ packages/mz-client/src/types/global.d.ts
@@ -1,7 +1,22 @@
 declare global {
+  interface Window {
+    beforeInstallPromptEvent: BeforeInstallPromptEvent;
+  }
+  interface Navigator {
+    standalone?: boolean;
+  }
   interface Document {
     startViewTransition(callback: () => void): void; // or any
   }
+}
+
+export interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+    outcome: 'accepted' | 'dismissed';
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
 }
 
 export const startViewTransition = document.startViewTransition; // or export {};
