dde3cac0f7d2b929d0ff7ba422bdaa02902df32c
shseok <shseok0674@gmail.com>
2024-01-29 23:40:54 +0900

    fix: getWeekRangeFromDate bug

--- packages/mz-client/src/lib/week.ts
+++ packages/mz-client/src/lib/week.ts
@@ -1,27 +1,19 @@
 import { format } from 'date-fns';
 
+// 입력 날짜가 포함된 주의 시작 날짜와 끝 날짜를 나타내는 배열을 반환
 export function getWeekRangeFromDate(d: Date) {
   const day = d.getDay();
   const startDate = d.getDate() - day;
 
   const start = format(new Date(d.setDate(startDate)), 'yyyy-MM-dd');
-  let end = format(
+  const end = format(
     new Date(new Date(start).setDate(startDate + 6)),
     'yyyy-MM-dd'
   );
-
-  if (+start.split('-')[2] > +end.split('-')[2]) {
-    const endArr = end.split('-');
-    endArr[1] =
-      `${+endArr[1] + 1}`.length === 1
-        ? `0${+endArr[1] + 1}`
-        : `${+endArr[1] + 1}`;
-    end = endArr.join('-');
-  }
   return [start, end];
 }
 
-export function getCalcDay(date = new Date(), days: number) {
+function getCalcDay(date = new Date(), days: number) {
   const next = new Date(date.getTime());
   next.setDate(date.getDate() + days);
 
