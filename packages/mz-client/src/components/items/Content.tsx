2a752f8a95127238c8d660938463d695a3d5c6ec
shseok <shseok0674@gmail.com>
2024-02-15 05:02:55 +0900

    fix: [WIP] convert to an element with style applied to Content

--- packages/mz-client/src/components/items/Content.tsx
+++ packages/mz-client/src/components/items/Content.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import type { OutputBlockData } from '@editorjs/editorjs';
 import { extractDomain } from '@/utils/extractDomain';
+import styles from '@/styles/Content.module.scss';
+import { cn } from '@/utils/common';
 
 interface Props {
   block: OutputBlockData<string, any>;
 }
 export default function Content({ block }: Props) {
-  let content = '';
+  console.log('block', block);
+  let content;
   let text;
   let align;
   let listItems = '';
@@ -16,7 +19,8 @@ export default function Content({ block }: Props) {
       console.log('paragraph', block);
       text = block.data.text;
       align = block.tunes?.textAlignment?.alignment;
-      content = `<div class="ce-block__content"><p style="text-align: ${align}">${text}</p></div>`;
+      content = <p className={cn(styles.paragraph, styles[align])}>{text}</p>;
+      // content = `<div class="ce-block__content"><p style="text-align: ${align}">${text}</p></div>`;
       break;
     }
     case 'header': {
@@ -24,7 +28,12 @@ export default function Content({ block }: Props) {
       const level = block.data.level;
       text = block.data.text;
       align = block.tunes?.textAlignment?.alignment;
-      content = `<div class="ce-block__content"><h${level} style="text-align: ${align}">${text}</h${level}></div>`;
+      content = (
+        <p className={cn(styles.header, styles[`h${level}`], styles[align])}>
+          {text}
+        </p>
+      );
+      // content = `<div class="ce-block__content"><h${level} style="text-align: ${align}">${text}</h${level}></div>`;
       break;
     }
     case 'alert': {
@@ -33,18 +42,34 @@ export default function Content({ block }: Props) {
       text = block.data.message || '빈 내용입니다';
       align = block.data.align;
       // TODO: cdx- 이렇게 붙여주고 있는데, 따로 컴포넌트로 만들어서 붙여주는게 좋을듯
-      content = `<div class="ce-block__content"><div class="cdx-alert cdx-alert-${type}" style="text-align: ${align}">${text}</div></div>`;
+      // content = `<div class="ce-block__content"><div class="cdx-alert cdx-alert-${type}" style="text-align: ${align}">${text}</div></div>`;
+      content = (
+        <div
+          className={cn(styles.alert, styles[`alert-${type}`], styles[align])}
+        >
+          {text}
+        </div>
+      );
       break;
     }
     case 'list': {
       console.log('list', block);
       const style = block.data.style;
-      block.data.items.map((item: string) => {
-        listItems += `<li class="cdx-list__item">${item}</li>`;
-      });
-      style === 'unordered'
-        ? (content = `<ul class="cdx-block cdx-list cdx-list--unordered">${listItems}</ul>`)
-        : (content = `<ol class="cdx-block cdx-list cdx-list--ordered">${listItems}</ol>`);
+      // block.data.items.map((item: string) => {
+      //   listItems += `<li class="cdx-list__item">${item}</li>`;
+      // });
+      // style === 'unordered'
+      //   ? (content = `<ul class="cdx-block cdx-list cdx-list--unordered">${listItems}</ul>`)
+      //   : (content = `<ol class="cdx-block cdx-list cdx-list--ordered">${listItems}</ol>`);
+      content = (
+        <ul
+          className={cn(styles.list, style === 'unordered' && styles.unordered)}
+        >
+          {block.data.items.map((item: string, index: number) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      );
       break;
     }
     // return (
@@ -56,23 +81,38 @@ export default function Content({ block }: Props) {
     // );
     case 'checkList': {
       console.log('checklist', block);
-      block.data.items.map((item: { text: string; checked: boolean }) => {
-        listItems += `<div class="cdx-checklist__item ${
-          item.checked && 'cdx-checklist__item--checked'
-        }">
-        <div class="cdx-checklist__item-checkbox"><span class="cdx-checklist__item-checkbox-check"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M7 12L10.4884 15.8372C10.5677 15.9245 10.705 15.9245 10.7844 15.8372L17 9"></path></svg></span></div>
-        <div class="cdx-checklist__item-text" contenteditable="true">${
-          item.text
-        }</div></div>`;
-      });
-      content = `
-      <div class="ce-block">
-        <div class="ce-block__content">
-          <div class="cdx-block cdx-checklist">
-          ${listItems}
+      // block.data.items.map((item: { text: string; checked: boolean }) => {
+      //   listItems += `<div class="cdx-checklist__item ${
+      //     item.checked && 'cdx-checklist__item--checked'
+      //   }">
+      //   <div class="cdx-checklist__item-checkbox"><span class="cdx-checklist__item-checkbox-check"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M7 12L10.4884 15.8372C10.5677 15.9245 10.705 15.9245 10.7844 15.8372L17 9"></path></svg></span></div>
+      //   <div class="cdx-checklist__item-text" contenteditable="true">${
+      //     item.text
+      //   }</div></div>`;
+      // });
+      // content = `
+      // <div class="ce-block">
+      //   <div class="ce-block__content">
+      //     <div class="cdx-block cdx-checklist">
+      //     ${listItems}
+      //     </div>
+      //   </div>
+      // </div>`;
+      // TODO: 보류
+      content = (
+        <div className={styles.block}>
+          <div className={styles.checklist}>
+            {block.data.items.map(
+              (item: { text: string; checked: boolean }, index: number) => (
+                <div key={index} className={styles.item}>
+                  <input type='checkbox' disabled checked={item.checked} />
+                  {item.text}
+                </div>
+              )
+            )}
           </div>
         </div>
-      </div>`;
+      );
       break;
     }
     // return (
@@ -137,7 +177,7 @@ src="${embed}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
         <div class="cdx-block">
           <div class="link-tool">
             <a class="link-tool__content link-tool__content--rendered" target="_blank" rel="nofollow noindex noreferrer" href="${link}">
-              <div class="link-tool__image" style="background-image: url(&quot;${image}&quot;);"></div>
+              <div class="link-tool__image" style="background-image: url(&quot;${image.url}&quot;);"></div>
               <div class="link-tool__title">${title}</div>
               <p class="link-tool__description">${description}</p>
               <span class="link-tool__anchor">${domain}</span>
@@ -151,5 +191,5 @@ src="${embed}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
       break;
     }
   }
-  return <div dangerouslySetInnerHTML={{ __html: content }} />;
+  return <div className={styles.block}>{content}</div>;
 }
